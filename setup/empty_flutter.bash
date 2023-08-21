88fd8b63c47199c340e85ff24c07901fab6daf0c
Nutchanon Ninyawee <nutchanon@codustry.com>
2023-08-21 13:53:29 +0700

    add flutter

---
+++ setup/empty_flutter.bash
@@ -0,0 +1,9 @@
+# desktop
+sudo apt install clang cmake ninja-build pkg-config libgtk-3-dev
+
+# chrome
+cd /tmp
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install --fix-broken -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
