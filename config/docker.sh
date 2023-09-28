4a90e001ed3391a9cf29a0c73ff0abcd8da92c7f
Nutchanon Ninyawee <nutchanon@codustry.com>
2023-09-28 10:16:17 +0700

    add docker config

--- config/docker.sh
+++ config/docker.sh
@@ -1,4 +1,15 @@
 # [not recommend] Set Port for docker
-if ! grep -q '^net.ipv4.ip_unprivileged_port_start=80$' /etc/sysctl.conf; then
-  echo 'net.ipv4.ip_unprivileged_port_start=80' | sudo tee -a /etc/sysctl.conf
-fi
+# if ! grep -q '^net.ipv4.ip_unprivileged_port_start=80$' /etc/sysctl.conf; then
+#   echo 'net.ipv4.ip_unprivileged_port_start=80' | sudo tee -a /etc/sysctl.conf
+# fi
+
+# allow to use port 80
+sudo setcap cap_net_bind_service=ep $(which rootlesskit)
+
+
+# config daemon
+systemctl --user start docker
+systemctl --user enable docker
+
+# prevent docker from being killed when logout
+sudo loginctl enable-linger $(whoami)
