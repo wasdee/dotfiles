fa808ebc5a00e6d053aa7a036621eff5050454e1
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-01-10 23:33:36 +0700

    Add OpenAI API Key and install dependencies for aider-chat

--- setup/aider-chat.bash
+++ setup/aider-chat.bash
@@ -1,1 +1,11 @@
-sudo apt install universal-ctags -y
+sudo apt install universal-ctags libportaudio2 -y
+
+pipx install aider-chat
+pipx inject aider-chat setuptools
+
+eval $(op signin)
+OPENAI_API_KEY=$(op read "op://Personal/Openai/API Key")
+
+touch ~/.aider.conf.yml
+# Add OpenAI API Key
+echo "openai-api-key: $OPENAI_API_KEY" >> ~/.aider.conf.yml
