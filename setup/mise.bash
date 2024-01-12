407db3bcd79f5d03fe11397e0d0c9782295c38c9
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-01-12 11:15:57 +0700

    Enhanced mise setup with activation script
    Integrated a new activation process for the mise tool within the setup
    script. This addition follows the instructions from the official
    getting-started guide, ensuring the tool is properly activated and
    configured for use with the Nu shell. The update includes appending the
    activation command output to the Nu shell configuration file for
    seamless integration.

--- setup/mise.bash
+++ setup/mise.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-
+# https://mise.jdx.dev/getting-started.html
 curl https://mise.jdx.dev/install.sh | sh
+
+# do {
+#   let misepath = ($nu.config-path | path dirname | path join "mise.nu")
+#   run-external mise activate nu --redirect-stdout | save $misepath -f
+#   $"\nsource "($misepath)"" | save $nu.config-path --append
+# }
