97526b04e8738f9c4e835965fd187e873f491e55
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-01-19 23:19:36 +0700

    Add environment variables and update completion scripts

--- setup/RegenCompletion.bash
+++ setup/RegenCompletion.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # use to regen all completions
-
+# ./RegenCompletion.bash
+# pre-reqs: run at setup dir 
 set -e
+
 
 mise completions bash > ../dot_bash_completions/mise.bash
 just --completions bash > ../dot_bash_completions/just.bash
+gh completion -s bash > ../dot_bash_completions/gh.bash
