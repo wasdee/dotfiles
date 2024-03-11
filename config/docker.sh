7b53ab58fb727cd6c5789da310a71cbef75dc3ab
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-03-11 14:23:00 +0000

    update docker

--- config/docker.sh
+++ config/docker.sh
@@ -2,6 +2,29 @@
 # if ! grep -q '^net.ipv4.ip_unprivileged_port_start=80$' /etc/sysctl.conf; then
 #   echo 'net.ipv4.ip_unprivileged_port_start=80' | sudo tee -a /etc/sysctl.conf
 # fi
+sudo apt-get install -y dbus-user-session
+sudo apt-get install -y uidmap
+curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh ./get-docker.sh --dry-run
+sudo sh ./get-docker.sh
+
+dockerd-rootless-setuptool.sh install
+## NOTES
+# [INFO] Installed docker.service successfully.
+# [INFO] To control docker.service, run: `systemctl --user (start|stop|restart) docker.service`
+# [INFO] To run docker.service on system startup, run: `sudo loginctl enable-linger nam`
+
+# [INFO] Creating CLI context "rootless"
+# Successfully created context "rootless"
+# [INFO] Using CLI context "rootless"
+# Current context is now "rootless"
+
+# [INFO] Make sure the following environment variable(s) are set (or add them to ~/.bashrc):
+# export PATH=/usr/bin:$PATH
+
+# [INFO] Some applications may require the following environment variable too:
+# export DOCKER_HOST=unix:///run/user/1002/docker.sock
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 
 # allow to use port 80
 sudo setcap cap_net_bind_service=ep $(which rootlesskit)
