f74dd75708bb1be53f1b578f38b9cc9f215f97d2
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-11-14 16:04:53 +0700

    complete styling

--- font.ps1
+++ font.ps1
@@ -1,2 +1,6 @@
+# https://www.programmingfonts.org/
+
 # c:\Users\lifep\AppData\Local\Programs\cursor\resources\app\bin
 choco install nerd-fonts-JetBrainsMono
+
+# https://github.com/githubnext/monaspace/pull/89
