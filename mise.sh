95f967f1845e344d8a67df9c0f981c28dde81859
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-11-15 12:33:19 +0700

    add notes

---
+++ mise.sh
@@ -0,0 +1,7 @@
+curl https://mise.run | sh
+echo "eval \"\$(/home/ben/.local/bin/mise activate bash)\"" >> ~/.bashrc
+sudo apt install unzip
+mise settings set experimental true
+sudo  apt install python3.12-venv
+mise use -g pipx
+mise use -g pipx:pgcli
