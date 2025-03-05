9067ac8f2447a6c6f4de847dcd086a05c88644b1
Nutchanon Ninyawee <nutchanon@codustry.com>
2025-03-05 20:09:19 +0700

    Add configuration files for editors, git, server, and setup

---
+++ config/editors.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# do not use `export` keyword in this file:
+# shellcheck disable=SC2034
+
+# Used by `setup-environment-commands`
+
+# Our editors in order of preference
+TERMINAL_EDITORS=(
+	'micro' # setup-util-micro - 18k stars
+	# editors with an obvious newb friendly UX
+	'nano' # setup-util-nano
+	# editors with an extremely obtuse UX:
+	# requires more than 5 seconds for a newb to save and quit a file
+	# sorted by popularity of conventions, then by modernness of the tool of that convention
+	'nvim'  # setup-util-neovim - 49k stars
+	'vim'   # setup-util-vim
+	'vi'    # commonly bundled, only worthwhile if vim is missing
+	'emacs' # setup-util-emacs - 3k stars
+	'amp'   # setup-util-amp - 3k stars
+	'ne'    # setup-util-ne - 300 stars
+)
+GUI_EDITORS=(
+	'cursor'
+	'code' # setup-util-vscode
+	'zed'  # https://zed.dev
+	'atom'
+	'subl'
+	'gedit'             # linux default
+	'gnome-text-editor' # fedora default
+	'TextEdit'          # macos default
+)
+
+# editors with complicated install instructions:
+# https://github.com/syl20bnr/spacemacs#install - 23.8k stars
+
+# dead editors:
+# https://github.com/slap-editor/slap/issues/413 - 6.1k stars
+# https://github.com/curlpipe/ox - 3.4k stars
+# https://github.com/gchp/iota - 1.6k stars
+# https://github.com/gphalkes/tilde - 414 stars
+
+# @todo add support for these:
+# https://github.com/helix-editor/helix - 35k stars - neovim
+# https://github.com/lapce/lapce - 34.9k stars - desktop, built on XI
+# https://github.com/NvChad/NvChad - 25.3k stars - neovim
+# https://github.com/syl20bnr/spacemacs - 23.8k stars - emacs
+# https://github.com/xi-editor/xi-editor - 19.8k stars - desktop, proof of concept
+# https://github.com/mawww/kakoune - 10.1k stars - neovim
+# https://github.com/jmacdonald/amp - 3.8k stars - GUI, proof of concept
+# https://github.com/ilai-deutel/kibi - 1.6k stars - modal
+# https://github.com/xyproto/orbiton - 500 stars - desktop
+# https://github.com/craigbarnes/dte - 164 stars - modal
---
+++ config/setup.bash
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+# do not use `export` keyword in this file:
+# shellcheck disable=SC2034
+
+# APK
+# Used by `setup-linux`
+# APK_INSTALL=()
+
+# Apt / apt-get
+# Used by `setup-linux`
+# APT_UNINSTALL=()
+# APT_INSTALL=()
+
+# AUR / pamac / pacman / yay / paru / pakku / aurutils
+# Used by `setup-linux`
+# AUR_INSTALL=()
+
+# Flatpak
+# Used by `setup-linux`
+# FLATPAK_INSTALL=()
+
+# RPM / dnf / yum
+# Used by `setup-linux`
+# RPM_INSTALL=()
+
+# Snap
+# Used by `setup-linux`
+# SNAP_INSTALL=()
+
+# Zypper
+# Used by `setup-linux`
+# ZYPPER_INSTALL=()
+
+# macOS App Store / mas / https://github.com/mas-cli/mas
+# Used by `setup-mac-appstore`
+# You can use `mas list` and `mas search` to find apps
+# MAS_INSTALL=() # tuple array of id, label
+# MAS_UPGRADE='no'
+
+# Homebrew / brew / https://brew.sh
+# Used by `setup-mac-brew`
+# You can use `setup-mac-brew --configure` to configure some of these.
+# HOMEBREW_UNTAPS=()
+# HOMEBREW_TAPS=()
+# HOMEBREW_FORMULAS=()
+# HOMEBREW_SLOW_FORMULAS=()
+# HOMEBREW_CASKS=()
+HOMEBREW_FONTS=(
+	'font-cantarell'
+	'font-cascadia-code'
+	'font-fira-code'
+	'font-fira-code-nerd-font'
+	'font-fira-mono'
+	'font-fira-mono-nerd-font'
+	'font-hack' # many editors require this
+	'font-hasklig'
+	'font-ibm-plex' # many editors require this
+	'font-inconsolata-go-nerd-font'
+	'font-inter' # many apps require this
+	'font-jetbrains-mono'
+	'font-jetbrains-mono-nerd-font'
+	'font-lato'
+	'font-maven-pro'
+	'font-monoid'
+	'font-montserrat'
+	'font-open-sans'
+	'font-oxygen'
+	'font-oxygen-mono'
+	'font-roboto'
+	'font-roboto-mono'
+	'font-source-code-pro' # many editors require this
+	'font-ubuntu'
+)
+# HOMEBREW_UNINSTALL=()        # for casks and formulas
+# HOMEBREW_ENCODING_INSTALL='' # '', 'yes', 'no'
+
+# Golang / go
+# Used by `setup-go`
+# GO_LINTING_INSTALL='' # '', 'yes', 'no'
+# GO_INSTALL=()
+
+# Node.js
+# Used by `setup-node`
+# NPM_INSTALL=()
+# NODE_VERSIONS=()
+
+# Python
+# Used by `setup-python`
+# PYTHON_INSTALL=()
+# PIP_INSTALL=()
+# PYTHON2_PIP_INSTALL=()
+# PYTHON3_PIP_INSTALL=()
+# PIPX_INSTALL=()
+
+# Ruby
+# Used by `setup-ruby`
+# GEM_INSTALL=()
+
+# Rust / Cargo / Crates.io
+# Used by `setup-rust`
+# CARGO_INSTALL=()
+
+# Utilities to install, these are the [setup-util-*] scripts
+# Used by `setup-utils`
+# You can use `setup-utils --configure` to configure these.
+SETUP_UTILS=(
+	apt
+	bat
+	bottom
+	brew
+	curl
+	delta
+	devel
+	dust
+	eza
+	fd
+	file
+	fzf
+	git
+	gum
+	httpie
+	jq
+	lux
+	mas
+	micro
+	nano
+	nerd-fonts
+	netstat
+	procs
+	ripgrep
+	sd
+	shellcheck
+	shfmt
+	starship
+	tealdeer
+	tokei
+	tree
+	vim
+	zoxide
+)
